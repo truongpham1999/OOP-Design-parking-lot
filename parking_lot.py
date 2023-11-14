611190d7a2e0ba9c6fa6ab674fae7609ff0dd258
truongpham1999 <truongphamzero1999@gmail.com>
2023-11-14 20:06:51 +0700

    fix remove_vehicle in ParkingFloor class

--- parking_lot.py
+++ parking_lot.py
@@ -8,6 +8,22 @@ class Vehicle:
     def get_spot_size(self):
         return self._spot_size
 
+class MotorBike(Vehicle):
+    def __init__(self):
+        super().__init__(1)
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__(2)
+
+class Limo(Vehicle):
+    def __init__(self):
+        super().__init__(3)
+
+class Truck(Vehicle):
+    def __init__(self):
+        super().__init__(4)
+
 class Driver:
     def __init__(self, id, vehicle):
         self._id = id
@@ -20,46 +36,40 @@ class Driver:
     def get_id(self):
         return self._id
 
-    def charge(self, amount):
+    def get_charge(self, amount):
         self._payment_due += amount
-
-class Car(Vehicle):
-    def __init__(self):
-        super().__init__(1)
-
-class Limo(Vehicle):
-    def __init__(self):
-        super().__init__(2)
-
-class SemiTruck(Vehicle):
-    def __init__(self):
-        super().__init__(3)
 
 class ParkingFloor:
     def __init__(self, spot_count):
-        self._spots = [0]*spot_count
+        self._spots = [0] * spot_count
         self._vehicle_map = {}
 
     def park_vehicle(self, vehicle):
         size = vehicle.get_spot_size()
-        l, r = 0, 0
-        while r < len(self._spots):
-            if self._spots[r] != 0:
-                l = r + 1
-            if r - l + 1 == size:
-                # we found enough spots, park the vehicle
-                for k in range(l, r+1):
-                    self._spots[k] = 1
-                self._vehicle_map[vehicle] = [l, r]
+        left, right = 0, 0
+
+        while right < len(self._spots):
+            if self._spots[right] != 0:
+                left = right + 1
+            # found enought spots for vehicle
+            if right - left + 1 == size:
+                for index in range(left, right + 1):
+                    self._spots[index] = 1
+                self._vehicle_map[vehicle] = [left, right]
                 return True
-            r += 1
+            right += 1
+
         return False
 
     def remove_vehicle(self, vehicle):
+        if vehicle not in self._vehicle_map:
+            return False
+
         start, end = self._vehicle_map[vehicle]
-        for i in range(start, end + 1):
-            self._spots[i] = 0
+        for index in range(start, end + 1):
+            self._spots[index] = 0
         del self._vehicle_map[vehicle]
+        return True
 
     def get_parking_spots(self):
         return self._spots
@@ -79,8 +89,7 @@ class ParkingGarage:
 
     def remove_vehicle(self, vehicle):
         for floor in self._parking_floors:
-            if floor.get_vehicle_spots(vehicle):
-                floor.remove_vehicle(vehicle)
+            if floor.remove_vehicle(vehicle):
                 return True
         return False
 
@@ -88,7 +97,7 @@ class ParkingSystem:
     def __init__(self, parkingGarage, hourlyRate):
         self._parkingGarage = parkingGarage
         self._hourlyRate = hourlyRate
-        self._timeParked = {} # map driverId to time that they parked
+        self._timeParked = {}    # map driverID to time that they parked
 
     def park_vehicle(self, driver):
         currentHour = datetime.datetime.now().hour
@@ -100,24 +109,30 @@ class ParkingSystem:
     def remove_vehicle(self, driver):
         if driver.get_id() not in self._timeParked:
             return False
-        currentHour = datetime.datetime.now().hour
-        timeParked = math.ceil(currentHour - self._timeParked[driver.get_id()])
-        driver.charge(timeParked * self._hourlyRate)
+        currentTime = datetime.datetime.now().hour
+        timeParked = math.ceil(currentTime - self._timeParked[driver.get_id()])
+        driver.get_charge(timeParked * self._hourlyRate)
 
         del self._timeParked[driver.get_id()]
         return self._parkingGarage.remove_vehicle(driver.get_vehicle())
 
-parkingGarage = ParkingGarage(3, 2)
+
+parkingGarage = ParkingGarage(2, 5)
 parkingSystem = ParkingSystem(parkingGarage, 5)
 
-driver1 = Driver(1, Car())
-driver2 = Driver(2, Limo())
-driver3 = Driver(3, SemiTruck())
+driverMotor = Driver(1, MotorBike())
+driverCar = Driver(2, Car())
+driverTruck_1 = Driver(8, Truck())
+driverTruck_2 = Driver(6, Truck())
 
-print(parkingSystem.park_vehicle(driver1))      # true
-print(parkingSystem.park_vehicle(driver2))      # true
-print(parkingSystem.park_vehicle(driver3))      # false
+print(parkingSystem.park_vehicle(driverTruck_1))    # true
+print(parkingSystem.park_vehicle(driverCar))        # true
+print(parkingSystem.park_vehicle(driverMotor))      # true
+print(parkingSystem.park_vehicle(driverTruck_2))    # false
 
-print(parkingSystem.remove_vehicle(driver1))    # true
-print(parkingSystem.remove_vehicle(driver2))    # true
-print(parkingSystem.remove_vehicle(driver3))    # false
+print(parkingSystem.remove_vehicle(driverTruck_1))  # true
+print(parkingSystem.remove_vehicle(driverCar))      # true
+print(parkingSystem.remove_vehicle(driverTruck_2))  # false
+
+
+
